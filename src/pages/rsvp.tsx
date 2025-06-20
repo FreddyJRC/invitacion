973fdd05c65b941748574d717b3d63fcd5065e9a
Freddy Ramirez <fj.ram.contr@gmail.com>
2025-06-19 22:02:01 -0600

    feat: query for person count

--- src/pages/rsvp.tsx
+++ src/pages/rsvp.tsx
@@ -1,15 +1,23 @@
 import Menu from "../menu"
 
 export default function Rsvp() {
+  let query = new URLSearchParams(window.location.search)
 
   return (
     <>
       <Menu name="RSVP" />
       <div className="flex flex-1 flex-col items-center justify-center">
-        <iframe
-          src="https://docs.google.com/forms/d/e/1FAIpQLSdK7TPESa5DWcXaIYVdQAqyRk_UFxcw9YEp9k-IehyAvzQFTg/viewform?embedded=true"
-          style={{ width: '100%', height: '80vh' }}
-        >Cargando…</iframe>
+        {query.get("p") === "2" ?
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSdK7TPESa5DWcXaIYVdQAqyRk_UFxcw9YEp9k-IehyAvzQFTg/viewform?embedded=true"
+            style={{ width: '100%', height: '90vh' }}
+          >Cargando…</iframe>
+        : 
+          <iframe 
+            src="https://docs.google.com/forms/d/e/1FAIpQLSdRK2PrNEWEtwbo0gVdreA75ACQx8stwR8mivefCxZlVIszaQ/viewform?embedded=true" 
+            style={{ width: '100%', height: '90vh' }}
+          >Cargando…</iframe>
+        }
       </div>
     </>
   )
